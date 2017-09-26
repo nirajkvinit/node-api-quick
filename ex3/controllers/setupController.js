148801f7bc798b8c356b5154ac8c21fd86f120cb
Niraj Kumar <nirajkvinit@gmail.com>
2017-09-26 19:26:56 +0530

    Basic Authorization for API implemented

--- ex3/controllers/setupController.js
+++ ex3/controllers/setupController.js
@@ -2,7 +2,25 @@ module.exports = function(server, plugins, restifyValidator) {
 	server.use(plugins.acceptParser(server.acceptable));
 	server.use(plugins.bodyParser());
 	server.use(plugins.queryParser());
+	server.use(plugins.authorizationParser());
 	server.use(restifyValidator);
+	server.use(function(req, res, next) {
+		var apiKeys = {
+			'user1': 'kjkjh5KJHv694JG745f667jfRR'
+		};
+		if(typeof(req.authorization.basic) === 'undefined' || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password !== apiKeys[req.authorization.basic.username]) {
+			var response = {
+				'status': 'failure',
+				'data': 'You must specify a valid API key.'
+			};
+			res.setHeader('content-type', 'application/json');
+			res.writeHead(403);
+			res.end(JSON.stringify(response));
+			//return next();
+		} else {
+			return next();	
+		}
+	});
 }
 
 
